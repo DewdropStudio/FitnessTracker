44c28187186ca6771563d159f63f70f4032cfe1e
Rocio Avila <rocio.avila@northwestern.com>
2020-09-05 11:57:38 -0500

    Revert to 3000 port

--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,30 @@
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(morgan("dev"));
+app.use(logger("dev"));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
-})
+app.use(express.static("public"));
 
-require("./routes/api")(app);
-require("./routes/views")(app);
+const URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
 
-app.listen(PORT,function(){ 
-    console.log(`App listening on Port ${PORT}`);
+mongoose.connect(URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api"));
+app.use(require("./routes/views"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
