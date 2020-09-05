e21b7cd6fe756ac246eb1ab3aa63eae71c8b82bb
Rocio Avila <rocio.avila@northwestern.com>
2020-09-04 20:22:59 -0500

    Chaged port to 8080

--- Develop/server.js
+++ Develop/server.js
@@ -1,30 +1,25 @@
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false
+})
 
-const URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+require("./routes/api")(app);
+require("./routes/views")(app);
 
-mongoose.connect(URI, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-// routes
-app.use(require("./routes/api"));
-app.use(require("./routes/views"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(PORT,function(){ 
+    console.log(`App listening on Port ${PORT}`);
 });
